c8b8a1040a09cb5b2b30a34825a6e3dcc8560a1b
leo03164 <leo03164@gmail.com>
2021-09-07 21:39:31 +0800

    feat: add ItemCard component

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="createFormContainer">
+    <Form :model="formItem" :label-width="80">
+      <div class="upload-container">
+        <ImageUpload @setImageIpfsPath="setImageIpfsPath"></ImageUpload>
+      </div>
+      <div class="input-form">
+        <FormItem label="UID">
+          <Input v-model="formItem.UID" placeholder="Enter manager UID"></Input>
+        </FormItem>
+        <FormItem label="Address">
+          <Input
+            v-model="formItem.address"
+            placeholder="Enter manager address"
+          ></Input>
+        </FormItem>
+        <FormItem label="Country">
+          <Select
+            v-model="formItem.country"
+            filterable
+            placeholder="Select manager country"
+            not-found-text="Country not found"
+          >
+            <Option v-for="data in countryList" :key="data" :value="data">{{
+              data
+            }}</Option>
+          </Select>
+        </FormItem>
+        <FormItem>
+          <div class="button-group">
+            <Button type="primary" @click="addManager">Submit</Button>
+            <Button style="margin-left: 8px" @click="cancel">Cancel</Button>
+          </div>
+        </FormItem>
+      </div>
+    </Form>
+  </div>
+</template>
+<script>
+import contry from "../shoes/country.json";
+import ImageUpload from "./ImageUpload.vue";
+import { mapState } from "vuex";
+
+export default {
+  name: "ItemCard",
+  data() {
+    return {
+      countryList: contry,
+      formItem: {
+        UID: "",
+        address: "",
+        country: ""
+      },
+      imgPath: ""
+    };
+  },
+  computed: {
+    ...mapState(["contract"])
+  },
+  components: {
+    ImageUpload
+  },
+  methods: {
+    async addManager() {
+      try {
+        console.log(this.formItem);
+        // set type 0x2 because of EIP1599
+        await this.contract.methods
+          .addShoesManager(
+            this.formItem.UID,
+            this.formItem.address,
+            this.formItem.country
+          )
+          .send({ type: "0x2" });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    cancel() {
+      this.$emit("close");
+    },
+    setImageIpfsPath(path) {
+      this.imgPath = path;
+    }
+  },
+  destroyed() {
+    window.localStorage.clear();
+  }
+};
+</script>
+<style lang="postcss" scoped>
+.createFormContainer {
+  width: 450px;
+  border-radius: 15px;
+  box-sizing: border-box;
+  padding: 50px;
+
+  position: fixed;
+  left: calc(100vh - 450px / 2);
+  top: 15vh;
+  float: left;
+
+  z-index: 999px;
+  background-color: #fff;
+
+  box-shadow: 3px 3px 2px rgba(0, 0, 0, 0.3);
+}
+.input-form {
+  margin-top: 20px;
+}
+.upload-container {
+  width: 100%;
+  height: 150px;
+  /* border-bottom: 1px solid #707070; */
+  box-shadow: 2px 3px 5px 0px #e2e2e2;
+}
+.button-group {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
