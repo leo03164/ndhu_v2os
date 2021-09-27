f9d2487b6b2565835eeb2398b158af7c235d6f44
leo03164 <leo03164@gmail.com>
2021-09-27 20:19:34 +0800

    feat: add ui component

---
+++ src/ui-components/ActionSheet.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="createFormContainer">
+    <div class="formTitle">{{ title }}</div>
+    <Form :label-width="80">
+      <div class="input-form">
+        <slot></slot>
+        <FormItem>
+          <div class="button-group">
+            <Button type="primary" @click="submit">Submit</Button>
+            <Button style="margin-left: 8px" @click="cancel">Cancel</Button>
+          </div>
+        </FormItem>
+      </div>
+    </Form>
+  </div>
+</template>
+<script>
+export default {
+  name: "ActionSheet",
+  props: {
+    title: String
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    submit() {
+      this.$emit("submit");
+    },
+    cancel() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+<style lang="postcss" scoped>
+.createFormContainer {
+  width: 450px;
+  border-radius: 15px;
+  box-sizing: border-box;
+  padding: 50px;
+
+  position: fixed;
+  left: calc(100vh - 450px / 2);
+  top: 15vh;
+  float: left;
+
+  z-index: 999px;
+  background-color: #fff;
+
+  box-shadow: 3px 3px 2px rgba(0, 0, 0, 0.3);
+}
+.formTitle {
+  text-align: center;
+  font-size: 24px;
+}
+.input-form {
+  margin-top: 20px;
+}
+.button-group {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
