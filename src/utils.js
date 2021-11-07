4ffebef41353c80ed281ea95f85a545b9d3d7427
leo03164 <leo03164@gmail.com>
2021-11-07 08:54:05 +0800

    refactor: change getShoesId calculate from blockChain to local

--- src/utils.js
+++ src/utils.js
@@ -4,4 +4,10 @@ export function timestampToDate(timestamp) {
   const month = String(date.getMonth() + 1);
   const day = String(date.getDate());
   return `${year}-${month}-${day}`;
-};
+}
+
+export function getShoesIdByAttribute(SN, name, company) {
+  const shoesAttributeString = `${SN}${name}${company}`;
+  const shoesId = web3.utils.keccak256(shoesAttributeString);
+  return shoesId;
+}
