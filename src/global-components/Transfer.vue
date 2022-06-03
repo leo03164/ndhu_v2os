209e8e8481cc7573342b136fd57e93e790d6a60a
leo03164 <leo03164@gmail.com>
2022-06-03 08:31:51 +0800

    Rename mode to tradeMode

--- src/global-components/Transfer.vue
+++ src/global-components/Transfer.vue
@@ -6,13 +6,13 @@
       @submit="transferHandler"
     >
       <FormItem label="轉移模式">
-        <RadioGroup v-model="mode">
+        <RadioGroup v-model="tradeMode">
           <Radio label="face2Face">轉移至買家</Radio>
           <Radio label="proxy">轉移至代理</Radio>
         </RadioGroup>
       </FormItem>
 
-      <FormItem label="購買者" v-if="mode === 'face2Face'">
+      <FormItem label="購買者" v-if="tradeMode === 'face2Face'">
         <Input v-model="buyer" placeholder="請輸入購買者位址"></Input>
       </FormItem>
 
@@ -45,7 +45,7 @@ export default {
   },
   data() {
     return {
-      mode: "face2Face",
+      tradeMode: "face2Face",
       buyer: "",
       price: 0,
       shoesId: ""
@@ -57,11 +57,11 @@ export default {
   methods: {
     async transferHandler() {
       try {
-        if (this.mode === "face2Face") {
+        if (this.tradeMode === "face2Face") {
           await this.contract.methods
             .transferShoesByOwner(this.shoesId, this.buyer, "Taiwan")
             .send({ type: "0x2" });
-        } else if (this.mode === "proxy") {
+        } else if (this.tradeMode === "proxy") {
           await this.contract.methods
             .transferShoesToContract(this.shoesId, this.price, "Taiwan")
             .send({ type: "0x2" });
